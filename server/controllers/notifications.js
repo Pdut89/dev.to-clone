e9ccb4973aca0adcc851c39dcb0e6aefaaadae4c
“Pdut89” <pdut8901@gmail.com>
2024-08-26 19:35:27 +1000

    refactor: insert console error loggging

--- server/controllers/notifications.js
+++ server/controllers/notifications.js
@@ -1,157 +1,162 @@
-const mongoose = require('mongoose');
-const Post = require('../models/post');
-const User = require('../models/user');
-const Notification = require('../models/notification');
-const HttpError = require('../models/http-error');
+const Notification = require('../models/notification')
+const HttpError = require('../models/http-error')
 
 const getAllNotifications = async (req, res, next) => {
-  let { userId } = req.params;
-  let notifications;
-  try {
-    await Notification.updateMany({ receiver: userId }, { read: true });
-    notifications = await Notification.find({ receiver: userId })
-      .sort({ date: 'desc' })
-      .populate('receiver')
-      .populate('sender')
-      .populate('post')
-      .populate('comment', 'body');
-  } catch (err) {
-    return next(
-      new HttpError('Could not fetch notifications, please try again', 500)
-    );
-  }
-  res.json({
-    notifications: notifications.map((notification) =>
-      notification.toObject({ getters: true })
-    ),
-  });
-};
+	try {
+		const { userId } = req.params
+		await Notification.updateMany({ receiver: userId }, { read: true })
+		const notifications = await Notification.find({ receiver: userId })
+			.sort({ date: 'desc' })
+			.populate('receiver')
+			.populate('sender')
+			.populate('post')
+			.populate('comment', 'body')
+
+		res.json({
+			notifications: notifications.map((notification) =>
+				notification.toObject({ getters: true })
+			),
+		})
+	} catch (error) {
+		console.error(error)
+		return next(
+			new HttpError('Could not fetch notifications, please try again', 500)
+		)
+	}
+}
 
 const getUnreadNotifications = async (req, res, next) => {
-  let { userId } = req.params;
-  let notifications;
-  try {
-    notifications = await Notification.find({
-      receiver: userId,
-      read: false,
-    })
-      .populate('receiver')
-      .populate('sender')
-      .populate('post')
-      .populate('comment', 'body');
-  } catch (err) {
-    return next(
-      new HttpError('Could not fetch notifications, please try again', 500)
-    );
-  }
-  res.json({
-    notifications: notifications.map((notification) =>
-      notification.toObject({ getters: true })
-    ),
-  });
-};
+	try {
+		const { userId } = req.params
+		const notifications = await Notification.find({
+			receiver: userId,
+			read: false,
+		})
+			.populate('receiver')
+			.populate('sender')
+			.populate('post')
+			.populate('comment', 'body')
+
+		res.json({
+			notifications: notifications.map((notification) =>
+				notification.toObject({ getters: true })
+			),
+		})
+	} catch (error) {
+		console.error(error)
+		return next(
+			new HttpError('Could not fetch notifications, please try again', 500)
+		)
+	}
+}
 
 const likeNotification = async (senderId, postId, receiverId, next) => {
-  //senderId : logged in user
-  //receiverId : the user to notify
-  try {
-    const createdNotification = new Notification({
-      notificationType: 'like',
-      sender: senderId,
-      receiver: receiverId,
-      post: postId,
-    });
-    await createdNotification.save();
-    return;
-  } catch (err) {
-    return new HttpError('Could not create the like notification', 500);
-  }
-};
+	//senderId : logged in user
+	//receiverId : the user to notify
+	try {
+		const createdNotification = new Notification({
+			notificationType: 'like',
+			sender: senderId,
+			receiver: receiverId,
+			post: postId,
+		})
+		await createdNotification.save()
+		return
+	} catch (error) {
+		console.error(error)
+		return new HttpError('Could not create the like notification', 500)
+	}
+}
 
 //remove the notification when like has been deleted
 const removeLikeNotification = async (senderId, postId, receiverId, next) => {
-  try {
-    await Notification.findOneAndDelete({
-      receiver: receiverId,
-      notificationType: 'like',
-      sender: senderId,
-      post: postId,
-    });
-    return;
-  } catch (err) {
-    return new HttpError('Could not delete the notification', 500);
-  }
-};
+	try {
+		await Notification.findOneAndDelete({
+			receiver: receiverId,
+			notificationType: 'like',
+			sender: senderId,
+			post: postId,
+		})
+		return
+	} catch (error) {
+		console.error(error)
+		return new HttpError('Could not delete the notification', 500)
+	}
+}
 
 const commentNotification = async (senderId, postId, commentId, receiverId) => {
-  try {
-    const createdNotification = new Notification({
-      notificationType: 'comment',
-      sender: senderId,
-      receiver: receiverId,
-      post: postId,
-      comment: commentId,
-    });
-    await createdNotification.save();
-    return;
-  } catch (err) {
-    return new HttpError('Could not create the notification', 500);
-  }
-};
+	try {
+		const createdNotification = new Notification({
+			notificationType: 'comment',
+			sender: senderId,
+			receiver: receiverId,
+			post: postId,
+			comment: commentId,
+		})
+		await createdNotification.save()
+		return
+	} catch (error) {
+		console.error(error)
+		return new HttpError('Could not create the notification', 500)
+	}
+}
 
 //remove the notification when comment has been deleted
 const removeCommentNotification = async (
-  senderId,
-  postId,
-  commentId,
-  receiverId
+	senderId,
+	postId,
+	commentId,
+	receiverId
 ) => {
-  try {
-    await Notification.findOneAndDelete({
-      receiver: receiverId,
-      notificationType: 'comment',
-      sender: senderId,
-      post: postId,
-      comment: commentId,
-    });
-    return;
-  } catch (err) {
-    return new HttpError('Could not delete the notification', 500);
-  }
-};
+	try {
+		await Notification.findOneAndDelete({
+			receiver: receiverId,
+			notificationType: 'comment',
+			sender: senderId,
+			post: postId,
+			comment: commentId,
+		})
+		return
+	} catch (error) {
+		console.error(error)
+		return new HttpError('Could not delete the notification', 500)
+	}
+}
 
 const followNotification = async (senderId, receiverId) => {
-  try {
-    const createdNotification = new Notification({
-      receiver: receiverId,
-      notificationType: 'follow',
-      sender: senderId,
-    });
-    await createdNotification.save();
-    return;
-  } catch (err) {
-    return new HttpError('Could not create the notification', 500);
-  }
-};
+	try {
+		const createdNotification = new Notification({
+			receiver: receiverId,
+			notificationType: 'follow',
+			sender: senderId,
+		})
+		await createdNotification.save()
+		return
+	} catch (error) {
+		console.error(error)
+		return new HttpError('Could not create the notification', 500)
+	}
+}
 
 const removeFollowNotification = async (senderId, receiverId) => {
-  try {
-    await Notification.findOneAndDelete({
-      receiver: receiverId,
-      notificationType: 'follow',
-      sender: senderId,
-    });
-    return;
-  } catch (err) {
-    return new HttpError('Could not delete the notification', 500);
-  }
-};
+	try {
+		await Notification.findOneAndDelete({
+			receiver: receiverId,
+			notificationType: 'follow',
+			sender: senderId,
+		})
+		return
+	} catch (error) {
+		console.error(error)
+		return new HttpError('Could not delete the notification', 500)
+	}
+}
 
-exports.likeNotification = likeNotification;
-exports.removeLikeNotification = removeLikeNotification;
-exports.commentNotification = commentNotification;
-exports.removeCommentNotification = removeCommentNotification;
-exports.followNotification = followNotification;
-exports.removeFollowNotification = removeFollowNotification;
-exports.getAllNotifications = getAllNotifications;
-exports.getUnreadNotifications = getUnreadNotifications;
+exports.likeNotification = likeNotification
+exports.removeLikeNotification = removeLikeNotification
+exports.commentNotification = commentNotification
+exports.removeCommentNotification = removeCommentNotification
+exports.followNotification = followNotification
+exports.removeFollowNotification = removeFollowNotification
+exports.getAllNotifications = getAllNotifications
+exports.getUnreadNotifications = getUnreadNotifications
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,41 +1,42 @@
-const jwt = require('jsonwebtoken');
-const DatauriParser = require('datauri/parser');
-const parser = new DatauriParser();
+const jwt = require('jsonwebtoken')
+const DataUriParser = require('datauri/parser')
+const path = require('path')
 
-const path = require('path');
-require('dotenv').config();
-const { JWT_KEY } = process.env;
-const { cloudinary } = require('../config/cloudinary');
+require('dotenv').config()
+const { JWT_KEY } = process.env
+
+const { cloudinary } = require('../config/cloudinary')
+
+const parser = new DataUriParser()
 
 const uploadToCloudinary = async (file) => {
-  try {
-    const extName = path.extname(file.originalname).toString();
-    const file64 = parser.format(extName, file.buffer);
+	try {
+		const extName = path.extname(file.originalname).toString()
+		const file64 = parser.format(extName, file.buffer)
 
-    const uploadedResponse = await cloudinary.uploader.upload(file64.content, {
-      upload_preset: 'devto',
-    });
-    return uploadedResponse.url;
-  } catch (err) {
-    console.log(err);
-  }
-};
+		const uploadedResponse = await cloudinary.uploader.upload(file64.content, {
+			upload_preset: 'devto',
+		})
+		return uploadedResponse.url
+	} catch (error) {
+		console.log(error)
+	}
+}
 
 const createJWTtoken = (id, email) => {
-  let jwtToken;
-  try {
-    jwtToken = jwt.sign(
-      //takes payload (the data you want to encode)
-      { userId: id, email: email },
-      JWT_KEY,
-      { expiresIn: '1h' } //token expires in 1 hr
-    );
-  } catch (err) {
-    console.log(err); //return err ('Signup failed, please try again', 500)
-    //'Login failed, please try again', 500)
-  }
-  return jwtToken;
-};
+	try {
+		const jwtToken = jwt.sign(
+			//takes payload (the data you want to encode)
+			{ userId: id, email: email },
+			JWT_KEY,
+			{ expiresIn: '1h' } //token expires in 1 hr
+		)
+		return jwtToken
+	} catch (error) {
+		console.log(error) //return err ('Signup failed, please try again', 500)
+		//'Login failed, please try again', 500)
+	}
+}
 
-exports.uploadToCloudinary = uploadToCloudinary;
-exports.createJWTtoken = createJWTtoken;
+exports.uploadToCloudinary = uploadToCloudinary
+exports.createJWTtoken = createJWTtoken
