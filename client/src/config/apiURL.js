d5cb8b45fb6931d1758c2cbc8bede896ecd0967e
“Pdut89” <pdut8901@gmail.com>
2024-09-01 17:54:01 +1000

    chore: remove api host default

--- client/src/config/apiURL.js
+++ client/src/config/apiURL.js
@@ -1,13 +1,22 @@
+// const {
+// 	REACT_APP_API_SCHEME: scheme = 'http',
+// 	REACT_APP_API_HOST: host = 'localhost',
+// 	REACT_APP_API_PORT: port = 5000,
+// 	REACT_APP_API_BASE_PATH: path = 'api',
+// } = process.env
+
 const {
-	REACT_APP_API_SCHEME: scheme = 'http',
-	REACT_APP_API_HOST: host = 'localhost',
-	REACT_APP_API_PORT: port = 5000,
-	REACT_APP_API_BASE_PATH: path = 'api',
+	REACT_APP_API_SCHEME: scheme,
+	REACT_APP_API_HOST: host,
+	REACT_APP_API_PORT: port,
+	REACT_APP_API_BASE_PATH: path,
 } = process.env
 
 const BASE = `${scheme}://${host}:${port}`
 
 const API_URL = `${BASE}/${path}`
+
+console.log('API URL: ', API_URL)
 
 export const SOCKET_IO_URL = BASE
 
