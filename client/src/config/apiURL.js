943efa62180cfca17a94c87be3abab9815f64af6
“Pdut89” <pdut8901@gmail.com>
2024-09-01 14:31:58 +1000

    fix: missing config file

---
+++ client/src/config/apiURL.js
@@ -0,0 +1,14 @@
+const {
+	REACT_APP_API_SCHEME: scheme = 'http',
+	REACT_APP_API_HOST: host = 'localhost',
+	REACT_APP_API_PORT: port = 5000,
+	REACT_APP_API_BASE_PATH: path = 'api',
+} = process.env
+
+const BASE = `${scheme}://${host}:${port}`
+
+const API_URL = `${BASE}/${path}`
+
+export const SOCKET_IO_URL = BASE
+
+export default API_URL
